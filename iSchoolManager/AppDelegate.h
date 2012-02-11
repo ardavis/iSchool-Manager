4288f97b6ddeb24ec9903cc5ba17aaefebbd969a
Andrew Davis <andrew@andrewrdavis.net>
2012-02-11 14:54:22 -0500

    Initial Commit

---
+++ iSchoolManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iSchoolManager
+//
+//  Created by Andrew Davis on 2/11/12.
+//  Copyright (c) 2012 NASA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iSchoolManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iSchoolManager
+//
+//  Created by Andrew Davis on 2/11/12.
+//  Copyright (c) 2012 NASA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
